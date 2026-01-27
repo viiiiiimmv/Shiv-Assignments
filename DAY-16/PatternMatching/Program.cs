8c6dfaefd22f5029db174784d4324c468a024711
VIII.III.MMV <shiv.chauhan1518@gmail.com>
2026-01-27 12:29:16 +0000

    Commit 1.1

---
+++ DAY-16/PatternMatching/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PatternMatching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your score : ");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            string grade = score switch
+            {
+                >= 90 => "A",
+                >= 80 => "B",
+                >= 70 => "C",
+                >= 60 => "D",
+                _ => "F"
+            };
+            Console.WriteLine($"Grade for score {score} is {grade}");
+        }
+    }
+}
---
+++ DAY-16/Threads/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Threads
+{
+    
+    internal class Program
+    {
+        public static void Func1()
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Function 1 prints {i}");
+            }
+        }
+
+        public static void Func2()
+        {
+            for (int i = 6; i > 0; i--)
+            {
+                Console.WriteLine($"Function 2 prints {i}");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            ThreadStart t1 = new ThreadStart(Func1);
+            ThreadStart t2 = new ThreadStart(Func2);
+            Thread first = new Thread(t1);
+            Thread second = new Thread(t2);
+            first.Start();
+            second.Start();
+
+            first.Priority = ThreadPriority.Lowest;
+            second.Priority = ThreadPriority.Highest;
+
+            Console.WriteLine();
+            Func1();
+            Func2();
+
+        }
+    }
+}
