86d5a48d5318bbeee1d19b8c0e835f82137f0b78
Shiv Chauhan <shiv.chauhan1518@gmail.com>
2026-01-28 14:53:28 +0530

    Commit 2.1

---
+++ ASSIGNMENT-DAY-17/PROBLEM-2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PROBLEM_2;
+
+class Program
+{
+    public static double VeryBigSum(int N, string[] arr)
+    {
+        double sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            sum += double.Parse(arr[i]);
+        }
+        
+        return sum;
+    }
+    static void Main(string[] args)
+    {
+        string [] arr = {"1000000001","1000000002","1000000003","1000000004","1000000005","1000000006","1000000007","1000000008","1000000009"};
+        Console.WriteLine(VeryBigSum(9, arr));
+    }
+}
---
+++ ASSIGNMENT-DAY-17/PROBLEM-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PROBLEM_3;
+
+class Program
+{
+    public static bool ValidateNumberPlate(string number)
+    {
+        var pattern = @"^[A-Z]{2}\d{2}[A-Z]{2}\d{4}$";
+        return Regex.IsMatch(number, pattern);
+    }
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        bool output =  ValidateNumberPlate(str);
+
+        if (output)
+        {
+            Console.WriteLine($"{str} is valid");
+        }
+        else
+        {
+            Console.WriteLine($"{str} is not valid");
+        }
+    }
+}
