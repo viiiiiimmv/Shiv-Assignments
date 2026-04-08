3507da5e96894d9477fc383ac6e57b010dc26338
viiiiiimmv <shiv.chauhan1518@gmail.com>
2026-04-08 10:57:53 +0530

    UPDATES : APRIL 8

---
+++ WEEK-5/APRIL-8/Day64/ProductAPIClient/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+namespace ProductAPIClient.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public ProductsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ApiBaseUrl = _config["ApiSettings:BaseUrl"];
+            return View();
+        }
+    }
+}
